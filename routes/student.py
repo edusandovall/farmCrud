17844b713b1b4e1b273eed87d5d89987ffecdb2e
Eduardo Astaiza S <hidinge@gmail.com>
2023-03-30 00:29:42 +0000

    Create API

--- routes/student.py
+++ routes/student.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
 from models.student import Student
 from config.database import connection
+from schemas.student import studentEntity, listOfStudentEntity
 
 student_router = APIRouter()
 
+#Get all students
 @student_router.get('/students')
 async def find_all_students():
-    return connection.local.student.find()
+    return listOfStudentEntity(connection.local.student.find())
+
+#Creating a student
+@student_router.post('/students')
+async def create_student(student: Student):
+    connection.local.student.insert_one(dict(student))
+    return listOfStudentEntity(connection.local.student.find())
