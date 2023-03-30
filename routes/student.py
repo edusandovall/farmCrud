98b3d6fdfcec97942b997abb80c8ee5e27aea567
Eduardo Astaiza S <hidinge@gmail.com>
2023-03-30 01:23:34 +0000

    Update and delete

--- routes/student.py
+++ routes/student.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from models.student import Student
 from config.database import connection
 from schemas.student import studentEntity, listOfStudentEntity
+from bson import ObjectId
 
 student_router = APIRouter()
 
@@ -10,8 +11,29 @@ student_router = APIRouter()
 async def find_all_students():
     return listOfStudentEntity(connection.local.student.find())
 
+#Get one student with matching id
+@student_router.get('/students/{studentId}')
+async def find_student_by_id(studentId):
+    return studentEntity(connection.local.student.find_one({"_id": ObjectId(studentId)}))
+
 #Creating a student
 @student_router.post('/students')
 async def create_student(student: Student):
     connection.local.student.insert_one(dict(student))
     return listOfStudentEntity(connection.local.student.find())
+
+#Update a student
+@student_router.put('/students/{studentId}')
+async def update_student(studentId, student: Student):
+    #Find the student then update with given data
+    connection.local.student.find_one_and_update(
+        {"_id": ObjectId(studentId)},
+        {"$set": dict(student)}
+    )
+    return studentEntity(connection.local.student.find_one({"_id": ObjectId(studentId)}))
+
+#Delete a student
+@student_router.delete('/students/{studentId}')
+async def delete_student(studentId):
+    #Find the student, deletes it and returns the same student object
+    return studentEntity(connection.local.student.find_one_and_delete({"_id": ObjectId(studentId)}))
